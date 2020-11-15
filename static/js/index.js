08c1635848c7ee97f10accc06bdd4e2f83724c2d
CKdave <chirag.django@gmail.com>
2020-11-16 02:48:43 +0530

    first commit

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+let menuItems = document.getElementById('menu-items');
+
+menuItems.style.maxHeight = "0px";
+
+function menuToggle(params) {
+    if(menuItems.style.maxHeight == "0px"){
+        menuItems.style.maxHeight = "200px";
+    } else{
+        menuItems.style.maxHeight = "0px";
+    }
+}
